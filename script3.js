68131d5d53cbcf2905dac61f1f6a583c8f4fb5be
Thanishka eerla <thanishka1287@gmail.com>
2026-07-25 14:36:09 +0530

    Add files via upload

---
+++ script3.js
@@ -0,0 +1,23 @@
+//declaring variables
+let name="thanishka";
+let age=20;
+let cgpa=9.1;
+let isStudent=true;
+let city;
+let phone=null;
+
+//printing to console
+console.log(name);
+console.log(age);
+console.log(cgpa);
+console.log(isStudent);
+console.log(city);
+console.log(phone);
+
+//using typeof operator
+console.log(typeof name);
+console.log(typeof age);
+console.log(typeof cgpa);
+console.log(typeof isStudent);
+console.log(typeof city);
+console.log(typeof phone);
