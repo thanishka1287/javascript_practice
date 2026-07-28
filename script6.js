8b55a5b84937c5e280b0d14c6367dedf59f23123
Thanishka eerla <thanishka1287@gmail.com>
2026-07-28 18:53:41 +0530

    Add files via upload

---
+++ script6.js
@@ -0,0 +1,8 @@
+console.log(10 == "10");//prints true
+console.log(10 === "10");//prints false
+console.log(20 > 15);//prints true
+console.log(5 < 2);//prints false
+console.log(8 >= 8);//prints true
+console.log(9 <= 7);//prints false
+console.log(15 != 20);//prints true
+console.log(15 !=="15");//prints false
