67ff0ab62a2d9b15fa1d66bac3cd204c1c04fefb
Thanishka eerla <thanishka1287@gmail.com>
2026-08-02 18:32:35 +0530

    Add files via upload

---
+++ script8.js
@@ -0,0 +1,13 @@
+let cardInserted=true;
+let pinCorrect=true;
+if(cardInserted){
+    if(pinCorrect){
+        console.log("Transaction allowed");
+    }
+    else{
+       console.log("Wrong PIN");
+    }
+}
+else{
+    console.log("Insert Card"); 
+}
