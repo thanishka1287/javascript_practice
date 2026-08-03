e631db5233df0c42b695c3f05c84938c35724d77
Thanishka eerla <thanishka1287@gmail.com>
2026-08-03 19:26:02 +0530

    Add files via upload

---
+++ script9.js
@@ -0,0 +1,13 @@
+let choice=3;
+switch(choice){
+    case 1:
+        console.log("Pizza");break;
+    case 2:
+        console.log("Burger");break;
+    case 3:
+        console.log("Pasta");break;
+    case 4:
+        console.log("Sandwich");break;
+    default:
+        console.log("Invalid choice");
+}
