f7213962e011711b7391311ec6cd096a4e424e33
Thanishka eerla <thanishka1287@gmail.com>
2026-08-04 18:56:00 +0530

    Add files via upload

---
+++ script10.js
@@ -0,0 +1,24 @@
+for(let i=10;i>=1;i--){
+    console.log(i);
+}
+
+for(let i = 5; i <=50; i=i+5){
+    console.log(i);
+}
+
+for(let i=2;i<=10;i=i+2){
+    console.log(i);
+}
+
+//prints numbers from 1 to 20 even nos only
+for(let i=1;i<=10;i=i+2){
+    console.log(i);
+}
+
+//sum of 10 numbers
+let sum=0;
+for(let i=1;i<=10;i++){
+    sum=sum+i;
+}
+console.log(sum);
+
