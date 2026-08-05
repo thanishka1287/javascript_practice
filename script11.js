f865cfb0a0dfa0d5fb97dbbba4297cc8fec21f4f
Thanishka eerla <thanishka1287@gmail.com>
2026-08-05 19:13:52 +0530

    Add files via upload

---
+++ script11.js
@@ -0,0 +1,23 @@
+//5 table
+for(let i=1;i<=10;i++){
+    console.log("5 * " +i+ " = " +5*i);
+}
+
+//multiples of 7 between 1 and 100
+for(let i=7;i<=100;i+=7){
+    console.log(i);
+}
+
+//print numbers from 1 to 20 divisible by 3
+for(let i=0;i<=20;i++){
+    if(i%3==0){
+        console.log(i);
+    }
+}
+
+//print only even numbers from 1 to 20
+for(let i=1;i<=20;i++){
+    if(i%2==0){
+        console.log(i);
+    }
+}
