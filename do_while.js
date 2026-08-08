3d8236fe12661b41c2022109cd3d65e7053edd1a
Thanishka eerla <thanishka1287@gmail.com>
2026-08-08 16:39:48 +0530

    Add files via upload

---
+++ do_while.js
@@ -0,0 +1,27 @@
+//prints nos 1 to 5
+let i=1;
+do{
+    console.log(i);
+    i++;
+}while(i<=5);
+
+//print nos from 10 to 1
+let i = 10;
+do{
+    console.log(i);
+    i--;
+}while(i>=1);
+
+//even nos from 2 to 20
+let i=2;
+do{
+    console.log(i);
+    i=i+2;
+}while(i<=20);
+
+//mutliplication table of 7
+let i = 1;
+do{
+    console.log("7 x " + i + " = " + (7 * i));
+    i++;
+}while(i <= 10);
