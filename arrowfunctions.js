c6425dc0e48a01f4ed1319a6e73d5501b0fcb867
Thanishka eerla <thanishka1287@gmail.com>
2026-08-17 17:54:40 +0530

    Add files via upload

---
+++ arrowfunctions.js
@@ -0,0 +1,25 @@
+let greet=()=>{
+    console.log("HELLO");
+}
+greet();
+//output:HELLO
+
+let add=(a,b)=>{
+    return a+b;
+}
+console.log(add(12,8));
+//output:20
+
+let mutliply=(a,b)=>{
+    return a*b;
+}
+console.log(mutliply(5,6));
+//output:30
+
+let square=(n)=>n*n;
+console.log(square(5));
+//output:25
+
+let cube=(n)=>n*n*n*n;
+console.log(cube(3));
+//output:27
